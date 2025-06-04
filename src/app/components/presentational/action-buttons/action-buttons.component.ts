f3a5ade4bed9d5817afce6358fc9d2ddb10d0776
Manual do Front <contatomanualdofront@gmail.com>
2025-06-04 16:02:03 -0300

    feat: create discount button

--- src/app/components/presentational/action-buttons/action-buttons.component.ts
+++ src/app/components/presentational/action-buttons/action-buttons.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FeaturesService, FeatureState } from '../../../services/view/features.service';
 
 @Component({
   selector: 'app-action-buttons',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './action-buttons.component.html',
-  styleUrl: './action-buttons.component.scss'
+  styleUrl: './action-buttons.component.scss',
 })
 export class ActionButtonsComponent {
+  private readonly featuresService = inject(FeaturesService);
 
+  readonly featuresState$: Observable<FeatureState> = this.featuresService.featuresState$;
 }
